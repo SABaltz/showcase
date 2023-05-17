709ab336bf6bcae634d92f1a4c5b04b14bcd8070
sam <samuelabaltz@gmail.com>
2023-05-16 18:29:21 -0600

    Initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Box, CardMedia, createTheme, Stack, Typography} from "@mui/material";
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#cb1818',
+        }
+    },
+})
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <CardMedia component="img" sx={{height: '40rem'}}
+                       image={require('../src/pexels-adrien-olichon-2387819.jpg')}/>
+            <Box sx={{
+                scrollbarWidth: 0,
+                position: 'absolute',
+                color: 'white',
+                top: '40%',
+                left: '50%',
+                transform: 'translateX(-50%)'
+            }}>
+                <Stack>
+                    <Typography variant={'h2'} sx={{}}>
+                        Samuel Baltz
+                    </Typography>
+                    <Typography variant={'h2'} sx={{
+                        position: 'absolute',
+                        color: 'white',
+                        top: '40%',
+                        left: '50%',
+                        transform: 'translateX(-50%)'
+                    }}>
+                        Programmer
+                    </Typography>
+                </Stack>
+            </Box>
+        </>
+    );
 }
 
 export default App;
