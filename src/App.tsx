d5891f7c3c377227cd64b417a048ff7dc52f4b71
sam <samuelabaltz@gmail.com>
2023-05-16 18:33:10 -0600

    Add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Box, CardMedia, createTheme, Stack, Typography} from "@mui/material";
+import {Box, CardMedia, createTheme, Stack, ThemeProvider, Typography} from "@mui/material";
 
 const darkTheme = createTheme({
     palette: {
@@ -13,31 +13,33 @@ const darkTheme = createTheme({
 function App() {
     return (
         <>
-            <CardMedia component="img" sx={{height: '40rem'}}
-                       image={require('../src/pexels-adrien-olichon-2387819.jpg')}/>
-            <Box sx={{
-                scrollbarWidth: 0,
-                position: 'absolute',
-                color: 'white',
-                top: '40%',
-                left: '50%',
-                transform: 'translateX(-50%)'
-            }}>
-                <Stack>
-                    <Typography variant={'h2'} sx={{}}>
-                        Samuel Baltz
-                    </Typography>
-                    <Typography variant={'h2'} sx={{
-                        position: 'absolute',
-                        color: 'white',
-                        top: '40%',
-                        left: '50%',
-                        transform: 'translateX(-50%)'
-                    }}>
-                        Programmer
-                    </Typography>
-                </Stack>
-            </Box>
+            <ThemeProvider theme={darkTheme}>
+                <CardMedia component="img" sx={{height: '40rem'}}
+                           image={require('../src/pexels-adrien-olichon-2387819.jpg')}/>
+                <Box sx={{
+                    scrollbarWidth: 0,
+                    position: 'absolute',
+                    color: 'white',
+                    top: '40%',
+                    left: '50%',
+                    transform: 'translateX(-50%)'
+                }}>
+                    <Stack>
+                        <Typography variant={'h2'} sx={{}}>
+                            Samuel Baltz
+                        </Typography>
+                        <Typography variant={'h2'} sx={{
+                            position: 'absolute',
+                            color: 'white',
+                            top: '40%',
+                            left: '50%',
+                            transform: 'translateX(-50%)'
+                        }}>
+                            Programmer
+                        </Typography>
+                    </Stack>
+                </Box>
+            </ThemeProvider>
         </>
     );
 }
