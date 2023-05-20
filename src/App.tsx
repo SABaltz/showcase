cf30b4be4052461d3879f012936d20d54bd9ebd3
sam <samuelabaltz@gmail.com>
2023-05-20 17:27:28 -0600

    Implement router

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,30 @@ import Name from "./components/Name";
 import {darkTheme} from "./components/Theme";
 import Quote from "./components/Quote";
 import Portrait from "./components/Portrait";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
     return (
         <>
             <ThemeProvider theme={darkTheme}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={
+                            <>
+                                <NavBar></NavBar>
+                                <Name></Name>
+                                <Quote></Quote>
+                                <Portrait></Portrait>
+                            </>
+                        }>
 
-                <NavBar></NavBar>
-                <Name></Name>
-                <Quote></Quote>
-                <Portrait></Portrait>
-                
+                        </Route>
+                        {/*<Route path="/home2">*/}
+                        {/*    <Name></Name>*/}
+                        {/*</Route>*/}
+                    </Routes>
+                </BrowserRouter>
+
             </ThemeProvider>
         </>
     );
