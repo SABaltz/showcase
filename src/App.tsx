20ca786b66926ef3f63425ad500a2c0d1608e626
sam <samuelabaltz@gmail.com>
2023-06-20 12:33:15 -0600

    Create Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {ThemeProvider,} from "@mui/material";
 import {darkTheme} from "./components/Theme";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import NavBar from "./components/NavBar";
+import HomePage from "./components/HomePage";
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
                         <Route path="/" element={
                             <>
                                 <NavBar></NavBar>
+                                <HomePage></HomePage>
                             </>
                         }>
                         </Route>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,16 +6,21 @@ export default function NavBar() {
         <AppBar position="static">
             <Toolbar>
                 <Grid container>
-                    <Grid item>
+                    <Grid item xs={9}>
                         <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
                             Samuel
                         </Typography>
                     </Grid>
-                    <Grid item xs>
+                    <Grid item xs={3}>
                         <Grid container direction="row-reverse">
-                            <Grid item>
+                            <Grid item xs={6}>
                                 <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
                                     Work
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                                    About
                                 </Typography>
                             </Grid>
                         </Grid>
