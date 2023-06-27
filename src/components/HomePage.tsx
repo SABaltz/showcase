fcf09ceaa7ce2f1fa7fdbbb997a541f3eb6e858d
sam <samuelabaltz@gmail.com>
2023-06-27 13:30:54 -0600

    Add in svg

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,7 +1,13 @@
+import React from "react";
+import {Box} from "@mui/material";
+
 export default function HomePage() {
     return (
-        <div>
-
-        </div>
+        <Box sx={{backgroundColor: 'red', width: '100vw', height: '100vh'}}>
+            <svg width="100vw" height="100vh">
+                <polyline strokeLinejoin="round" stroke="black" strokeWidth="12" fill="none"
+                          points="120,150 150,50 180,150"/>
+            </svg>
+        </Box>
     )
 }
