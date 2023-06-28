6c413a1be43c40046d45dc8a12f49197ab7954be
sam <samuelabaltz@gmail.com>
2023-06-28 10:19:43 -0600

    Add in background photo

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import {Box} from "@mui/material";
 
+let image = require('../static/background3.jpg')
 export default function HomePage() {
     return (
-        <Box sx={{backgroundColor: 'red', width: '100vw', height: '100vh'}}>
-            <svg width="100vw" height="100vh">
-                <polyline strokeLinejoin="round" stroke="black" strokeWidth="12" fill="none"
-                          points="120,150 150,50 180,150"/>
-            </svg>
+        <Box
+            sx={{
+                backgroundImage: 'url(' + image + ')',
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                width: '100vw',
+                height: '100vh'
+            }}>
+
         </Box>
     )
 }
