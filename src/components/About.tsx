88684d83a44f8666c0a7e03039eafd17d73bed53
sam <samuelabaltz@gmail.com>
2023-07-21 13:28:09 -0600

    Add in photos for gallery

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,10 +13,10 @@ function srcset(image: string, size: number, rows = 1, cols = 1) {
 
 
 export default function About() {
-    let doctor1 = require('../static/background3.jpg')
+
     return (
         <ImageList
-            sx={{width: 500, height: 450}}
+            sx={{width: '100vw', height: 450}}
             variant="quilted"
             cols={4}
             rowHeight={121}
@@ -37,61 +37,42 @@ export default function About() {
 
 const itemData = [
     {
-        img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+        img: require('../static/arches.JPG'),
         title: 'Breakfast',
         rows: 2,
         cols: 2,
     },
     {
-        img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+        img: require('../static/cave.JPG'),
         title: 'Burger',
     },
     {
-        img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+        img: require('../static/fisher.JPG'),
         title: 'Camera',
     },
     {
-        img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+        img: require('../static/glacier.jpg'),
         title: 'Coffee',
         cols: 2,
     },
     {
-        img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+        img: require('../static/kayak.jpg'),
         title: 'Hats',
         cols: 2,
     },
     {
-        img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+        img: require('../static/ousel.jpg'),
         title: 'Honey',
         author: '@arwinneil',
         rows: 2,
         cols: 2,
     },
     {
-        img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+        img: require('../static/vets.jpg'),
         title: 'Basketball',
     },
     {
-        img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+        img: require('../static/yellowstone.JPG'),
         title: 'Fern',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-        title: 'Mushrooms',
-        rows: 2,
-        cols: 2,
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-        title: 'Tomato basil',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-        title: 'Sea star',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-        title: 'Bike',
-        cols: 2,
-    },
+    }
 ];
