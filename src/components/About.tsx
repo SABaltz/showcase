2858b5d0836958034cdf24d1d88d97a3238f9ba1
sam <samuelabaltz@gmail.com>
2023-07-21 13:32:34 -0600

    Adjust row spacing

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {ImageList, ImageListItem} from "@mui/material";
 
 
-function srcset(image: string, size: number, rows = 1, cols = 1) {
+function srcset(image: string, size: number, rows = 4, cols = 4) {
     return {
         src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
         srcSet: `${image}?w=${size * cols}&h=${
@@ -16,7 +16,7 @@ export default function About() {
 
     return (
         <ImageList
-            sx={{width: '100vw', height: 450}}
+            sx={{width: '50vw', height: 500}}
             variant="quilted"
             cols={4}
             rowHeight={121}
@@ -44,7 +44,7 @@ const itemData = [
     },
     {
         img: require('../static/cave.JPG'),
-        title: 'Columbia Mountain Cavv=e',
+        title: 'Columbia Mountain Cave',
     },
     {
         img: require('../static/fisher.JPG'),
@@ -54,24 +54,28 @@ const itemData = [
         img: require('../static/glacier.jpg'),
         title: 'Glacier National Park',
         cols: 2,
+        rows: 2,
     },
     {
         img: require('../static/kayak.jpg'),
         title: 'Sea Kayaking',
         cols: 2,
+        rows: 6,
     },
     {
         img: require('../static/ousel.jpg'),
         title: 'Ousel Peak',
-        rows: 2,
+        rows: 3,
         cols: 2,
     },
     {
         img: require('../static/vets.jpg'),
         title: 'Sam in Hat',
+        rows: 2,
     },
     {
         img: require('../static/yellowstone.JPG'),
         title: 'Yellow Stone National Park',
+        rows: 2,
     }
 ];
