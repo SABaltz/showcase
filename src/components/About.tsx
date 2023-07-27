c16d8d16f27189b2d82541cc2d265fe71d34e319
sam <samuelabaltz@gmail.com>
2023-07-27 11:13:07 -0600

    Cut Down Photos

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import {ImageList, ImageListItem} from "@mui/material";
+import {ImageList, ImageListItem, useMediaQuery} from "@mui/material";
+import {darkTheme} from "./Theme";
 
 export default function About() {
-
+    const smallScreen = useMediaQuery(darkTheme.breakpoints.up('sm'));
     return (
-        <ImageList sx={{width: '100vw', height: '100vh'}} cols={3} rowHeight={1000}>
+        <ImageList sx={{width: '100vw', height: '100vh'}} cols={smallScreen ? 3 : 1} rowHeight={1000}>
             {itemData.map((item) => (
                 <ImageListItem key={item.img}>
                     <img
@@ -24,43 +25,25 @@ const itemData = [
     {
         img: require('../static/arches.JPG'),
         title: 'Arches National Park ',
-        rows: 2,
-        cols: 2,
     },
     {
         img: require('../static/cave.JPG'),
         title: 'Columbia Mountain Cave',
     },
     {
-        img: require('../static/fisher.JPG'),
-        title: 'Fisher Towers',
-    },
-    {
         img: require('../static/glacier.jpg'),
         title: 'Glacier National Park',
-        cols: 2,
-        rows: 2,
-    },
-    {
-        img: require('../static/kayak.jpg'),
-        title: 'Sea Kayaking',
-        cols: 2,
-        rows: 6,
     },
     {
         img: require('../static/ousel.jpg'),
         title: 'Ousel Peak',
-        rows: 3,
-        cols: 2,
     },
     {
         img: require('../static/vets.jpg'),
         title: 'Sam in Hat',
-        rows: 2,
     },
     {
         img: require('../static/yellowstone.JPG'),
         title: 'Yellow Stone National Park',
-        rows: 2,
     }
 ];
