b011d7d0ecfc27f4b73f44fc642c4d51e2cad4b7
sam <samuelabaltz@gmail.com>
2023-07-27 11:23:52 -0600

    Create Centered Grid

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,22 +1,29 @@
 import React from 'react'
-import {ImageList, ImageListItem, useMediaQuery} from "@mui/material";
+import {Grid, ImageList, ImageListItem, useMediaQuery} from "@mui/material";
 import {darkTheme} from "./Theme";
 
 export default function About() {
     const smallScreen = useMediaQuery(darkTheme.breakpoints.up('sm'));
     return (
-        <ImageList sx={{width: '100vw', height: '100vh'}} cols={smallScreen ? 3 : 1} rowHeight={1000}>
-            {itemData.map((item) => (
-                <ImageListItem key={item.img}>
-                    <img
-                        src={`${item.img}?w=1000&h=1000&fit=crop&auto=format`}
-                        srcSet={`${item.img}?w=1000&h=1000&fit=crop&auto=format&dpr=2 2x`}
-                        alt={item.title}
-                        loading="lazy"
-                    />
-                </ImageListItem>
-            ))}
-        </ImageList>
+        <>
+            <Grid container>
+                <Grid item xs={6} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>Test</Grid>
+                <Grid item xs={6} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>Test</Grid>
+            </Grid>
+            <ImageList sx={{width: '100vw', height: '100vh'}} cols={smallScreen ? 3 : 1} rowHeight={1000}>
+                {itemData.map((item) => (
+                    <ImageListItem key={item.img}>
+                        <img
+                            src={`${item.img}?w=1000&h=1000&fit=crop&auto=format`}
+                            srcSet={`${item.img}?w=1000&h=1000&fit=crop&auto=format&dpr=2 2x`}
+                            alt={item.title}
+                            loading="lazy"
+                        />
+                    </ImageListItem>
+                ))}
+            </ImageList>
+
+        </>
     )
 }
 
