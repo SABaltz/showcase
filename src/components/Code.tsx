f49297ce57fc2f2edce9d3e4394525d2abf3f30c
sam <samuelabaltz@gmail.com>
2023-07-28 12:55:06 -0600

    Add in icons

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -3,6 +3,9 @@ import {Box, Divider, Typography, useMediaQuery} from "@mui/material";
 import {darkTheme} from "./Theme";
 import NavBar from "./nav/NavBar";
 import NavButton from "./nav/NavButton";
+import {FaAngular, FaNode, FaNpm, FaPython, FaReact} from "react-icons/fa";
+import {FaR} from "react-icons/fa6";
+import JavascriptIcon from '@mui/icons-material/Javascript';
 
 export default function Code() {
     let linearStart = '#232526'
@@ -21,9 +24,22 @@ export default function Code() {
             <Box sx={{
                 background: `linear-gradient(to bottom, ${linearStart}, #414345 );`,
             }}>
+                <Divider sx={{fontSize: '5rem'}}><Typography variant={'h1'}>Languages</Typography></Divider>
+                <FaPython/>
+                <FaR/>
+                <JavascriptIcon/>
                 <Divider sx={{fontSize: '5rem'}}><Typography variant={'h1'}>FrontEnd</Typography></Divider>
+                <FaReact/>
+                <FaAngular/>
+                <FaNpm/>
+                {/*Material Ui*/}
+                {/*SemanticCss*/}
+
                 <Divider sx={{fontSize: '5rem'}}><Typography variant={'h1'}>BackEnd</Typography></Divider>
+                <FaNode/>
+                {/*Express*/}
                 <Divider sx={{fontSize: '5rem'}}><Typography variant={'h1'}>Database</Typography></Divider>
+                
                 <Divider sx={{fontSize: '5rem'}}><Typography variant={'h1'}>Linux</Typography></Divider>
                 <Divider sx={{fontSize: '5rem'}}><Typography variant={'h1'}>Hardware</Typography></Divider>
             </Box>
